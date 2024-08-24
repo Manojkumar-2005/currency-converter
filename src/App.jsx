0e986a942c5359002bd27b0b00fb062aeef74c5c
MANOJKUMAR N <139373029+Manojkumar-2005@users.noreply.github.com>
2024-08-24 16:46:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
   const[amount,setAmount]=useState(1);
   const[fromCurrency,setFromCurrency]=useState("USD");
   const[toCurrency,setToCurrency]=useState("INR");
   const[convertedAmount,setConvertedAmount]=useState(null);
+  const[exchangeRate,setExchangeRate]=useState(null)
+  useEffect(()=>{
+    const getExchangeRate=async()=>{
+      try{
+        let url=`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+        const response=await axios.get(url) 
+        setExchangeRate(response.data.rates[toCurrency])
+      } catch(error){
+        console.error("Error Fetching Exchange Rate:",error);
+      }
+    }
+    getExchangeRate()
+  },[fromCurrency, toCurrency])
+
+  useEffect(()=>{
+    if(exchangeRate !== null){
+      setConvertedAmount((amount * exchangeRate).toFixed(2))
+    }
+  },[amount, exchangeRate])
+
+  const handleAmountChange=(e)=>{
+    const value=parseFloat(e.target.value)
+    setAmount(isNaN(value) ? 0 : value)
+  }
+
+  const handleFromCurrencyChange=(e)=>{
+    setFromCurrency(e.target.value)
+  }
+
+  const handleToCurrencyChange=(e)=>{
+    setToCurrency(e.target.value)
+  }
 
   return (
     <>
@@ -15,11 +48,11 @@ function App() {
         <h1>Currency Converter</h1>
         <div className='input-container'>
           <label htmlFor='amt'>Amount:</label>
-          <input type='number' id='amt' value={amount}/>
+          <input type='number' id='amt' value={amount} onChange={handleAmountChange}/>
         </div>
         <div className='input-container'>
           <label htmlFor='fromCurrency'>From Currency:</label>
-          <select id='fromCurrency' value={fromCurrency}>
+          <select id='fromCurrency' value={fromCurrency} onChange={handleFromCurrencyChange}>
             <option value="USD">USD - United States Dollar</option>
             <option value="EUR">EUR - Euro</option>
             <option value="GBP">GBP - British Pound Sterling</option>
@@ -34,7 +67,7 @@ function App() {
         </div>
         <div className='input-container'>
           <label htmlFor='toCurrency'>To Currency:</label>
-          <select id='toCurrency' value={toCurrency}>
+          <select id='toCurrency' value={toCurrency} onChange={handleToCurrencyChange}>
             <option value="USD">USD - United States Dollar</option>
             <option value="EUR">EUR - Euro</option>
             <option value="GBP">GBP - British Pound Sterling</option>
